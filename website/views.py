dffe841938067231865e8ab62a140b9cffa391ee
ArturIvanov228 <90447341+ArturIvanov228@users.noreply.github.com>
2024-03-11 19:01:56 +0300

    Update views.py

--- website/views.py
+++ website/views.py
@@ -7,4 +7,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
+    '''
+    Create a home page
+    '''
     return render_template("home.html", user=current_user)
